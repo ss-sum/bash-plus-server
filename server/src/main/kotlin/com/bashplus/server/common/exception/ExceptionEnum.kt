f01fad416a3a3b42c47469609eb478f7d62541b7
YejiGong <yejigong00@gmail.com>
2024-03-30 02:24:59 +0900

    Fix: Edit Exception Enum

--- server/src/main/kotlin/com/bashplus/server/common/exception/ExceptionEnum.kt
+++ server/src/main/kotlin/com/bashplus/server/common/exception/ExceptionEnum.kt
@@ -6,10 +6,10 @@ enum class ExceptionEnum(
         private val status: HttpStatus,
         private val code: Int,
         private var message: String? = null) {
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, 404, "Bad Request"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "Bad Request"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal Server Error"),
     ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, 403, "Unauthorized"),
-    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, 404, "Bad Request"),
+    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, 400, "Bad Request"),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 401, "Expired Token"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 401, "Invalid Token"),
     TOKEN_ERROR(HttpStatus.BAD_REQUEST, 400, "Token Error"), ;
