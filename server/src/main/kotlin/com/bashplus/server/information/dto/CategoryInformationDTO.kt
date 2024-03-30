dd6c07080ec3ce2f28dc7892a5da67b5e19ad59e
YejiGong <yejigong00@gmail.com>
2024-03-31 01:18:36 +0900

    Fix: Edit Request, Response form of Information domain

--- server/src/main/kotlin/com/bashplus/server/information/dto/CategoryInformationDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/information/dto/CategoryInformationDTO.kt
@@ -2,9 +2,11 @@ package com.bashplus.server.information.dto
 
 import com.bashplus.server.information.domain.Category
 
-class CategoryInformationDTO private constructor(val tid: Long) {
+class CategoryInformationDTO private constructor(val tid: Long, val category: String, val level: Int) {
     constructor(category: Category) :
             this(
-                    tid = category.tid ?: 0
+                    tid = category.tid ?: 0,
+                    category = category.category,
+                    level = category.level
             )
 }
--- server/src/main/kotlin/com/bashplus/server/information/repository/CategoryRepository.kt
+++ server/src/main/kotlin/com/bashplus/server/information/repository/CategoryRepository.kt
@@ -1,6 +1,8 @@
 package com.bashplus.server.information.repository
 
 import com.bashplus.server.information.domain.Category
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -8,5 +10,5 @@ import java.util.*
 @Repository
 open interface CategoryRepository : JpaRepository<Category, String> {
     fun findByCategoryAndLevel(category: String, level: Int): Optional<Category>
-    open fun findAllByCategoryIsLike(category: String): List<Category>
+    open fun findAllByCategoryIsLikeIgnoreCase(category: String, pageable: Pageable): Page<Category>
 }
