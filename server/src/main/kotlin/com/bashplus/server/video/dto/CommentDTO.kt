b3b80a97cc1e8588129b6ac806e365035a7233ec
YejiGong <yejigong00@gmail.com>
2024-03-31 01:01:29 +0900

    Fix: Edit Request, Response form of Video domain

--- server/src/main/kotlin/com/bashplus/server/video/dto/CommentDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/video/dto/CommentDTO.kt
@@ -1,9 +1,10 @@
 package com.bashplus.server.video.dto
 
 import com.bashplus.server.video.domain.Comment
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
-class CommentDTO private constructor(val vid: Long, val content: String, val createdAt: LocalDateTime) {
+class CommentDTO private constructor(val vid: Long, val content: String, @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC") val createdAt: LocalDateTime) {
     constructor(comment: Comment) :
             this(
                     vid = comment.video.vid ?: 0,
--- server/src/main/kotlin/com/bashplus/server/video/dto/CommentRequestDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/video/dto/CommentRequestDTO.kt
@@ -2,9 +2,6 @@ package com.bashplus.server.video.dto
 
 class CommentRequestDTO {
     var uid: Long = 0
-        private set
     var vid: Long = 0
-        private set
     var content: String = ""
-        private set
 }
