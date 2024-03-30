c2f65b3f6ae346bacb4c622c6a68919379d44053
YejiGong <yejigong00@gmail.com>
2024-03-31 01:18:36 +0900

    Fix: Edit Request, Response form of Information domain

--- server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
+++ server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
@@ -5,6 +5,8 @@ import com.bashplus.server.information.service.InformationService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "정보 API", description = "목록 조회 관련 API")
@@ -16,15 +18,17 @@ class InformationController {
 
     @Operation(summary = "컨퍼런스 주최 목록 API", description = "검색 시 선택지 제공을 위한 목록 API")
     @GetMapping("/conference/hosts")
-    fun getHosts(): ResponseDTO {
-        val result = informationService.getConferenceHosts()
+    fun getHosts(@RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = informationService.getConferenceHosts(pageable)
         return ResponseDTO(result)
     }
 
     @Operation(summary = "카테고리 목록 API", description = "검색 시 선택지 제공을 위한 목록 API")
     @GetMapping("/conference/category")
-    fun getCategory(): ResponseDTO {
-        val result = informationService.getCategories()
+    fun getCategory(@RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = informationService.getCategories(pageable)
         return ResponseDTO(result)
     }
 
