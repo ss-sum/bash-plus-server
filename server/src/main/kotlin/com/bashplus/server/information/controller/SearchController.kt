dd9c81d6d039d4ee341fb01d96a0adb49ab7b91a
YejiGong <yejigong00@gmail.com>
2024-03-31 01:18:36 +0900

    Fix: Edit Request, Response form of Information domain

--- server/src/main/kotlin/com/bashplus/server/information/controller/SearchController.kt
+++ server/src/main/kotlin/com/bashplus/server/information/controller/SearchController.kt
@@ -5,10 +5,9 @@ import com.bashplus.server.information.service.SearchService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "검색 API", description = "검색 관련 API")
 @RestController
@@ -19,31 +18,36 @@ class SearchController {
 
     @Operation(summary = "전체 영상 목록 조회 API", description = "")
     @GetMapping("/conference/video")
-    fun getWholeVideos(): ResponseDTO {
-        val result = searchService.getAllVideos()
+    fun getWholeVideos(@RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = searchService.getAllVideos(pageable)
         return ResponseDTO(result)
     }
 
 
     @Operation(summary = "컨퍼런스 주최 검색 API", description = "")
     @GetMapping("/conference/host/{keyword}")
-    fun searchHost(@PathVariable keyword: String): ResponseDTO {
-        val result = searchService.getHostSearchResult(keyword)
+    fun searchHost(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = searchService.getHostSearchResult(keyword, pageable)
         return ResponseDTO(result)
     }
 
     @Operation(summary = "컨퍼런스 카테고리 검색 API", description = "")
     @GetMapping("/category/{keyword}")
-    fun searchCategory(@PathVariable keyword: String): ResponseDTO {
-        val result = searchService.getCategorySearchResult(keyword)
+    fun searchCategory(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = searchService.getCategorySearchResult(keyword, pageable)
         return ResponseDTO(result)
     }
 
     @Operation(summary = "컨퍼런스 비디오 제목 검색 API", description = "")
     @GetMapping("/conference/video/{keyword}")
-    fun searchConference(@PathVariable keyword: String): ResponseDTO {
-        val result = searchService.getVideoSearchResult(keyword)
+    fun searchConference(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+        val pageable: Pageable = PageRequest.of(pageNum, pageSize)
+        val result = searchService.getVideoSearchResult(keyword, pageable)
         return ResponseDTO(result)
     }
+
 
 }
