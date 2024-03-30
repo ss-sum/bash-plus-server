fa11bd724a065684d37e59c3c1455711fe48ebcc
YejiGong <yejigong00@gmail.com>
2024-03-31 00:47:35 +0900

    Fix: Edit Request, Response form of User domain

--- server/src/main/kotlin/com/bashplus/server/users/dto/InterestRequestDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/users/dto/InterestRequestDTO.kt
@@ -2,7 +2,5 @@ package com.bashplus.server.users.dto
 
 class InterestRequestDTO(category: String, level: Int) {
     var category: String = category ?: ""
-        private set
     var level: Int = level ?: 0
-        private set
 }
