28ed0105862b0c24b532f21ccf52c3902ca856c1
YejiGong <yejigong00@gmail.com>
2024-03-31 00:22:13 +0900

    Fix: Edit Request, Response form of Archive domain

--- server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoDTO.kt
@@ -2,9 +2,13 @@ package com.bashplus.server.archive.dto
 
 import com.bashplus.server.archive.domain.Archive
 
-class ArchiveVideoDTO private constructor(val vid: Long) {
+class ArchiveVideoDTO private constructor(val vid: Long, val title: String, val content: String, val url: String, val conference: String) {
     constructor(archive: Archive) :
             this(
-                    vid = archive.video.vid ?: 0
+                    vid = archive.video.vid ?: 0,
+                    title = archive.video.title,
+                    content = archive.video.content ?: "",
+                    url = archive.video.url,
+                    conference = archive.video.conference.title
             )
 }
