cede1597f6000869cd40e9c1305b75f8212bbcab
YejiGong <yejigong00@gmail.com>
2024-04-01 11:33:00 +0900

    Fix: Add page info to response type

--- server/src/main/kotlin/com/bashplus/server/information/controller/SearchController.kt
+++ server/src/main/kotlin/com/bashplus/server/information/controller/SearchController.kt
@@ -1,6 +1,6 @@
 package com.bashplus.server.information.controller
 
-import com.bashplus.server.common.ResponseDTO
+import com.bashplus.server.common.ResponseListDTO
 import com.bashplus.server.information.service.SearchService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -18,35 +18,35 @@ class SearchController {
 
     @Operation(summary = "컨퍼런스별 전체 영상 목록 조회 API", description = "")
     @GetMapping("/conference/{keyword}/videos")
-    fun getWholeVideos(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+    fun getWholeVideos(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
         val result = searchService.getAllVideosByConference(keyword, pageable)
-        return ResponseDTO(result)
+        return result
     }
 
 
     @Operation(summary = "컨퍼런스 주최 검색 API", description = "")
     @GetMapping("/conference/host/{keyword}")
-    fun searchHost(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+    fun searchHost(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
         val result = searchService.getHostSearchResult(keyword, pageable)
-        return ResponseDTO(result)
+        return result
     }
 
     @Operation(summary = "컨퍼런스 카테고리 검색 API", description = "")
     @GetMapping("/category/{keyword}")
-    fun searchCategory(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+    fun searchCategory(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
         val result = searchService.getCategorySearchResult(keyword, pageable)
-        return ResponseDTO(result)
+        return result
     }
 
     @Operation(summary = "컨퍼런스 비디오 제목 검색 API", description = "")
     @GetMapping("/conference/video/{keyword}")
-    fun searchConference(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseDTO {
+    fun searchConference(@PathVariable keyword: String, @RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
         val result = searchService.getVideoSearchResult(keyword, pageable)
-        return ResponseDTO(result)
+        return result
     }
 
 
--- server/src/main/kotlin/com/bashplus/server/information/service/InformationService.kt
+++ server/src/main/kotlin/com/bashplus/server/information/service/InformationService.kt
@@ -1,5 +1,6 @@
 package com.bashplus.server.information.service
 
+import com.bashplus.server.common.ResponseListDTO
 import com.bashplus.server.host.repository.HostRepository
 import com.bashplus.server.information.dto.CategoryInformationDTO
 import com.bashplus.server.information.dto.HostInformationDTO
@@ -17,13 +18,13 @@ class InformationService {
     @Autowired
     private lateinit var categoryRepository: CategoryRepository
 
-    fun getConferenceHosts(page: Pageable): List<HostInformationDTO> {
-        val result = hostRepository.findAll(page).toList().map { host -> HostInformationDTO(host) }
-        return result
+    fun getConferenceHosts(page: Pageable): ResponseListDTO {
+        val result = hostRepository.findAll(page)
+        return ResponseListDTO(result.toList().map { host -> HostInformationDTO(host) }, page.pageNumber, page.pageSize, result.totalElements)
     }
 
-    fun getCategories(page: Pageable): List<CategoryInformationDTO> {
-        val result = categoryRepository.findAll(page).toList().map { category -> CategoryInformationDTO(category) }
-        return result
+    fun getCategories(page: Pageable): ResponseListDTO {
+        val result = categoryRepository.findAll(page)
+        return ResponseListDTO(result.toList().map { category -> CategoryInformationDTO(category) }, page.pageNumber, page.pageSize, result.totalElements)
     }
 }
