82b9b4d358b3cf7e11b3fc5007c37d5897ea748b
soyoungxxx <otherplet@gmail.com>
2024-04-11 01:01:04 +0900

    feat: Add swagger in Information

--- server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
+++ server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
@@ -1,8 +1,15 @@
 package com.bashplus.server.information.controller
 
 import com.bashplus.server.common.ResponseListDTO
+import com.bashplus.server.common.exception.ApiExceptionEntity
+import com.bashplus.server.information.dto.CategoryInformationDTO
+import com.bashplus.server.information.dto.HostInformationDTO
 import com.bashplus.server.information.service.InformationService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
@@ -17,6 +24,11 @@ class InformationController {
     private lateinit var informationService: InformationService
 
     @Operation(summary = "컨퍼런스 주최 목록 API", description = "검색 시 선택지 제공을 위한 목록 API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK", content = [Content(schema = Schema(implementation = HostInformationDTO::class))]),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))]),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))])
+    ])
     @GetMapping("/conference/hosts")
     fun getHosts(@RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
@@ -25,6 +37,11 @@ class InformationController {
     }
 
     @Operation(summary = "카테고리 목록 API", description = "검색 시 선택지 제공을 위한 목록 API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK", content = [Content(schema = Schema(implementation = CategoryInformationDTO::class))]),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))]),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))])
+    ])
     @GetMapping("/conference/categories")
     fun getCategory(@RequestParam pageSize: Int, @RequestParam pageNum: Int): ResponseListDTO {
         val pageable: Pageable = PageRequest.of(pageNum, pageSize)
