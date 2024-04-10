08620ab2a3584df3758929ad987997c8c3eb7d44
soyoungxxx <otherplet@gmail.com>
2024-04-11 00:51:23 +0900

    feat: Add swagger in Curation

--- server/src/main/kotlin/com/bashplus/server/curation/controller/CurationController.kt
+++ server/src/main/kotlin/com/bashplus/server/curation/controller/CurationController.kt
@@ -1,6 +1,8 @@
 package com.bashplus.server.curation.controller
 
 import com.bashplus.server.common.ResponseDTO
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/curation")
 class CurationController {
     @GetMapping("/video/user/{userId}/recommendation")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    ])
     fun getRecommendedVideos(@PathVariable userId: String): ResponseDTO {
         return ResponseDTO()
     }
