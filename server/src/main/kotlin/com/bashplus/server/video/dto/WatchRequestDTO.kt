9b6ae95767383dfdc4b0b9abc5af328e498d60e4
soyoungxxx <otherplet@gmail.com>
2024-04-15 18:21:49 +0900

    refactor: Edit Annotation at WatchRequestDTO.kt

--- server/src/main/kotlin/com/bashplus/server/video/dto/WatchRequestDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/video/dto/WatchRequestDTO.kt
@@ -5,12 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalTime
 
-@Schema(description = "영상 시청 시간", pattern = "HH:mm:ss", type = "String")
-@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC")
 class WatchRequestDTO {
     var uid: Long = 0
     var vid: Long = 0
 
+    @Schema(description = "영상 시청 시간", pattern = "HH:mm:ss", type = "String")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC")
     @DateTimeFormat(pattern = "HH:mm:ss")
     var time: LocalTime = LocalTime.now()
 }
