aa36cb6b3bcc08c2114d1dbe9fe38589108cc2bf
soyoungxxx <otherplet@gmail.com>
2024-04-15 18:20:18 +0900

    refactor: Edit ApiResponses at InfromationController

--- server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
+++ server/src/main/kotlin/com/bashplus/server/information/controller/InformationController.kt
@@ -8,6 +8,7 @@ import com.bashplus.server.information.service.InformationService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.media.SchemaProperty
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -25,7 +26,7 @@ class InformationController {
 
     @Operation(summary = "컨퍼런스 주최 목록 API", description = "검색 시 컨퍼런스 호스트 선택지 제공을 위한 목록 API")
     @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "OK", content = [Content(schema = Schema(implementation = HostInformationDTO::class))]),
+        ApiResponse(responseCode = "200", description = "OK", content = [Content(schemaProperties = [SchemaProperty(name = "message", schema = Schema(implementation = HostInformationDTO::class))])]),
         ApiResponse(responseCode = "400", description = "BAD REQUEST", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))]),
         ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))])
     ])
@@ -38,7 +39,7 @@ class InformationController {
 
     @Operation(summary = "카테고리 목록 API", description = "검색 시 카테고리 선택지 제공을 위한 목록 API")
     @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "OK", content = [Content(schema = Schema(implementation = CategoryInformationDTO::class))]),
+        ApiResponse(responseCode = "200", description = "OK", content = [Content(schemaProperties = [SchemaProperty(name = "message", schema = Schema(implementation = CategoryInformationDTO::class))])]),
         ApiResponse(responseCode = "400", description = "BAD REQUEST", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))]),
         ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = [Content(schema = Schema(implementation = ApiExceptionEntity::class))])
     ])
