88405b6e9e78cb506b2c7f5d1870ff116de50371
soyoungxxx <otherplet@gmail.com>
2024-04-15 15:07:44 +0900

    feat: Add annotation in VideoDTO

--- server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoRequestDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoRequestDTO.kt
@@ -1,8 +1,12 @@
 package com.bashplus.server.archive.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalTime
 
+@Schema(description = "영상 시간", pattern = "HH:mm:ss", type = "String")
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC")
 class ArchiveVideoRequestDTO {
     var vid: Long = 0
     var uid: Long = 0
--- server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoWatchRecordDTO.kt
+++ server/src/main/kotlin/com/bashplus/server/archive/dto/ArchiveVideoWatchRecordDTO.kt
@@ -2,8 +2,11 @@ package com.bashplus.server.archive.dto
 
 import com.bashplus.server.archive.domain.Archive
 import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalTime
 
+@Schema(description = "기록 날짜", pattern = "HH:mm:ss", type = "String")
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC")
 class ArchiveVideoWatchRecordDTO private constructor(val vid: Long, val title: String, val content: String, val url: String, val conference: String, @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC") val time: LocalTime) {
     constructor(archive: Archive) :
             this(
@@ -12,6 +15,7 @@ class ArchiveVideoWatchRecordDTO private constructor(val vid: Long, val title: S
                     content = archive.video.content ?: "",
                     url = archive.video.url,
                     conference = archive.video.conference.title,
+
                     time = archive.time ?: LocalTime.now()
             )
 }
